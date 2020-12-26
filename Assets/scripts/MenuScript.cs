e8ed7173bd0261d87c994a463a6a719fab2eb2bf
mbabayi574 <51171133+mbabayi574@users.noreply.github.com>
2020-12-26 17:27:36 +0330

    Design Mainmenu
    Design Mainmenu

---
+++ Assets/scripts/MenuScript.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuScript : MonoBehaviour
+{
+
+    public Button Playgame;
+    public Button AboutUs;
+    public Button Settings;
+    public Button Exit;
+
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        Playgame.onclick.AddListener(() =>
+        {
+            Application.LoadLevel("");
+
+        });
+        AboutUs.onclick.AddListener(() =>
+        {
+
+
+        });
+        Settings.onclick.AddListener(() =>
+        {
+
+
+        });
+        Exit.onclick.AddListener(() =>
+        {
+            Application.Quit;
+        
+
+        });
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
