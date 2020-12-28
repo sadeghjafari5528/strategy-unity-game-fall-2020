783d13ddf621a134866f1840efc10fd9bc2ca49e
mbabayi574 <51171133+mbabayi574@users.noreply.github.com>
2020-12-28 22:13:55 +0330

    Sadeghci

--- Assets/scripts/MenuScript.cs
+++ Assets/scripts/MenuScript.cs
@@ -1,9 +1,31 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Menuscript : MonoBehaviour
 {
+
+    public void Playgame()
+    {
+        SceneManager.LoadScene("SampleScene");
+    }
+
+    public void OpenSettings()
+    {
+        SceneManager.LoadScene("Settings");
+    }
+
+    public void LoadAboutus()
+    {
+        SceneManager.LoadScene("AboutUs");
+    }
+
+    public void Exitgame()
+    {
+        Application.Quit();
+    }
+    
     // Start is called before the first frame update
     void Start()
     {
